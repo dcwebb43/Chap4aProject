782b63296c1ebf904dfc03066a40d7e5838325c5
Dalyssa <Dalyssa@DESKTOP-QJUVPV9>
2019-03-07 15:59:06 -0600

    Add project files.

---
+++ Chap4aProject/frmAverageGrade.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Chap4aProject
+{
+    partial class frmAverageGrade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpTestScores = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore3 = new System.Windows.Forms.TextBox();
+            this.txtScore2 = new System.Windows.Forms.TextBox();
+            this.txtScore1 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.grpTestScores.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpTestScores
+            // 
+            this.grpTestScores.Controls.Add(this.label6);
+            this.grpTestScores.Controls.Add(this.label5);
+            this.grpTestScores.Controls.Add(this.lblGrade);
+            this.grpTestScores.Controls.Add(this.lblAverage);
+            this.grpTestScores.Controls.Add(this.label3);
+            this.grpTestScores.Controls.Add(this.label2);
+            this.grpTestScores.Controls.Add(this.label1);
+            this.grpTestScores.Controls.Add(this.txtScore3);
+            this.grpTestScores.Controls.Add(this.txtScore2);
+            this.grpTestScores.Controls.Add(this.txtScore1);
+            this.grpTestScores.Location = new System.Drawing.Point(64, 23);
+            this.grpTestScores.Name = "grpTestScores";
+            this.grpTestScores.Size = new System.Drawing.Size(147, 184);
+            this.grpTestScores.TabIndex = 0;
+            this.grpTestScores.TabStop = false;
+            this.grpTestScores.Text = "Enter test scores";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Grade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Average";
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGrade.Location = new System.Drawing.Point(79, 154);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(39, 20);
+            this.lblGrade.TabIndex = 7;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverage.Location = new System.Drawing.Point(79, 124);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(39, 20);
+            this.lblAverage.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Score 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Score 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score 1";
+            // 
+            // txtScore3
+            // 
+            this.txtScore3.Location = new System.Drawing.Point(79, 78);
+            this.txtScore3.Name = "txtScore3";
+            this.txtScore3.Size = new System.Drawing.Size(39, 20);
+            this.txtScore3.TabIndex = 2;
+            // 
+            // txtScore2
+            // 
+            this.txtScore2.Location = new System.Drawing.Point(79, 52);
+            this.txtScore2.Name = "txtScore2";
+            this.txtScore2.Size = new System.Drawing.Size(39, 20);
+            this.txtScore2.TabIndex = 1;
+            // 
+            // txtScore1
+            // 
+            this.txtScore1.Location = new System.Drawing.Point(79, 26);
+            this.txtScore1.Name = "txtScore1";
+            this.txtScore1.Size = new System.Drawing.Size(39, 20);
+            this.txtScore1.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(39, 266);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(65, 42);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate &Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Location = new System.Drawing.Point(171, 257);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(65, 24);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(171, 296);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(65, 24);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Location = new System.Drawing.Point(51, 216);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(0, 13);
+            this.lblFeedback.TabIndex = 4;
+            // 
+            // frmAverageGrade
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(275, 332);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.grpTestScores);
+            this.Name = "frmAverageGrade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DS3850: Chapter 4a Project";
+            this.grpTestScores.ResumeLayout(false);
+            this.grpTestScores.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpTestScores;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtScore3;
+        private System.Windows.Forms.TextBox txtScore2;
+        private System.Windows.Forms.TextBox txtScore1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFeedback;
+    }
+}
+
---
+++ Chap4aProject/frmAverageGrade.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chap4aProject
+{
+    public partial class frmAverageGrade : Form
+    {
+        public frmAverageGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // declare variables and constants...
+                decimal decTest1, decTest2, decTest3, decAvg;
+                const decimal decHighScore = 90, decLowScore = 70;
+                // erase old feedback
+                lblFeedback.Text = "";
+                // parse content of textboxes
+                decTest1 = decimal.Parse(txtScore1.Text);
+                decTest2 = decimal.Parse(txtScore2.Text);
+                decTest3 = decimal.Parse(txtScore3.Text);
+                // calculate average score
+                decAvg = (decTest1 + decTest2 + decTest3) / 3;
+                // determine letter grade
+                if (decAvg < 60)
+                {
+                    lblGrade.Text = "F";
+                }
+                else if (decAvg < 70)
+                {
+                    lblGrade.Text = "D";
+                }
+                else if (decAvg < 80)
+                {
+                    lblGrade.Text = "C";
+                }
+                else if (decAvg < 90)
+                {
+                    lblGrade.Text = "B";
+                }
+                else if (decAvg <= 100)
+                {
+                    lblGrade.Text = "A";
+                }
+                else // average is over 100
+                {
+                    lblGrade.Text = "Err";
+                }
+                // give feedback
+                if (decAvg >= decHighScore)
+                {
+                    lblFeedback.Text = "Good job!";
+                }
+                else if (decAvg >= decLowScore)
+                {
+                    lblFeedback.Text = "Keep trying!"; 
+                }
+                else // average is below decLowScore
+                {
+                    lblFeedback.Text = "Ask your instructor for help.";
+                }
+                // Check for possible TA position
+                if (decTest1 >= 90 && decTest2 >= 90 && decTest3 >= 90)
+                {
+                    MessageBox.Show("See instructor for possible TA position.");
+                }
+                // Ask student to see instructor about low score
+                if (decTest1 < 60 || decTest2 <60 || decTest3 < 60)
+                {
+                    MessageBox.Show("See instructor about your lowest score.");
+                }
+                // display output
+                lblAverage.Text = decAvg.ToString("n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Test scores must be numeric." + "\n" + ex.Message);
+            }
+        }
+    }
+}
